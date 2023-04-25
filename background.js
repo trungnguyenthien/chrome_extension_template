934609e3bb27213e555225cf2a8569738704520c
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-04-26 05:39:50 +0700

    UPDATED

---
+++ background.js
@@ -0,0 +1,57 @@
+// Hàm này được gọi khi sự kiện onCompleted xảy ra
+function onCompleted2(details) {
+  // Thực hiện đoạn mã JavaScript của bạn ở đây
+  // alert("Trang web mới đã tải xong:" + details.url)
+  chrome.cookies.getAll({domain: "youtube.com"}, function (cookies) {
+    var a = []
+    cookies.forEach(function(cookie) {
+      console.log(cookie.name, cookie.value);
+      a.push({key: cookie.name, value: cookie.value})
+    });
+    // console.log(cookies);
+    alert(a)
+});
+}
+
+// Đăng ký sự kiện onCompleted
+chrome.webNavigation.onCompleted.addListener(onCompleted2, {
+  url: [{
+    hostContains: '.'
+  }]
+});
+
+chrome.commands.onCommand.addListener(async function(command) {
+  if (command === "save_page") {
+    const tabs = await chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+    })
+    let url = tabs[0].url;
+    const _url = downloadUrl(url);
+    if (!_url) {
+      return
+    }
+    chrome.downloads.download({
+      url: _url,
+      saveAs: false,
+      conflictAction: "uniquify"
+    }, function (downloadId) {
+
+      chrome.downloads.onChanged.addListener(function (downloadDelta) {
+        if (downloadDelta.state && downloadDelta.state.current === "complete" && downloadDelta.id === downloadId) {
+          // xử lý khi download hoàn tất
+          console.log("Download complete!");
+        }
+      });
+    });
+  }
+});
+
+
+const api = 'http://localhost:3000/xcontent?file=1&url='
+const downloadUrl = (url) => {
+    if (!url.startsWith('https://truyensextv.me/')) {
+        return null;
+    }
+    return api + url
+}
---
+++ popup.js
@@ -0,0 +1,68 @@
+//const { default: axios } = require("axios");
+
+(async function initPopupWindow() {
+    const icLoading = $('#ic_loading');
+    const icError = $('#ic_error');
+    const icSuccess = $('#ic_success');
+
+    icLoading.attr('hidden', true);
+    icSuccess.attr('hidden', true);
+    icError.attr('hidden', true);
+
+    const tabs = await chrome.tabs.query({
+        active: true,
+        lastFocusedWindow: true
+    })
+    let url = tabs[0].url;
+
+
+    $("#url").text(url);
+
+    chrome.cookies.getAll({
+        domain: "youtube.com"
+    }, function (cookies) {
+        var cookieValue = ""
+        cookies.forEach(function (cookie) {
+            console.log(cookie.name, cookie.value);
+            cookieValue += (`${cookie.name}: ${cookie.value};`)
+        });
+        alert(cookieValue)
+    });
+
+    icLoading.attr('hidden', false);
+    const _url = downloadUrl(url);
+    if (!_url) {
+        return
+    }
+    chrome.downloads.download({
+        url: _url,
+        saveAs: false,
+        conflictAction: "uniquify"
+    }, function (downloadId) {
+
+        chrome.downloads.onChanged.addListener(function (downloadDelta) {
+            if (downloadDelta.id !== downloadId) return
+            if (!downloadDelta.state) return
+            if (downloadDelta.state.current === "complete") {
+                // xử lý khi download hoàn tất
+                console.log("Download complete!");
+                icLoading.attr('hidden', true);
+                icSuccess.attr('hidden', false);
+                icError.attr('hidden', true);
+            }
+
+            if (downloadDelta.error) {
+                // xử lý khi download hoàn tất
+                console.log("Download FAILT!");
+                icLoading.attr('hidden', true);
+                icSuccess.attr('hidden', true);
+                icError.attr('hidden', false);
+            }
+        });
+    });
+})();
+
+const api = 'http://localhost:3000/xcontent?file=1&url='
+const downloadUrl = (url) => {
+    return api + url
+}
