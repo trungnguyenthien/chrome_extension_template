573bca8b9055e85ee88520a19f38f9ea4e74da0f
Trung Nguyen Thien <ngthientrung@gmail.com>
2023-04-26 23:48:14 +0700

    UPDATED

--- background.js
+++ background.js
@@ -2,15 +2,6 @@
 function onCompleted2(details) {
   // Thực hiện đoạn mã JavaScript của bạn ở đây
   // alert("Trang web mới đã tải xong:" + details.url)
-  chrome.cookies.getAll({domain: "youtube.com"}, function (cookies) {
-    var a = []
-    cookies.forEach(function(cookie) {
-      console.log(cookie.name, cookie.value);
-      a.push({key: cookie.name, value: cookie.value})
-    });
-    // console.log(cookies);
-    alert(a)
-});
 }
 
 // Đăng ký sự kiện onCompleted
@@ -20,38 +11,6 @@ chrome.webNavigation.onCompleted.addListener(onCompleted2, {
   }]
 });
 
-chrome.commands.onCommand.addListener(async function(command) {
-  if (command === "save_page") {
-    const tabs = await chrome.tabs.query({
-      active: true,
-      lastFocusedWindow: true
-    })
-    let url = tabs[0].url;
-    const _url = downloadUrl(url);
-    if (!_url) {
-      return
-    }
-    chrome.downloads.download({
-      url: _url,
-      saveAs: false,
-      conflictAction: "uniquify"
-    }, function (downloadId) {
-
-      chrome.downloads.onChanged.addListener(function (downloadDelta) {
-        if (downloadDelta.state && downloadDelta.state.current === "complete" && downloadDelta.id === downloadId) {
-          // xử lý khi download hoàn tất
-          console.log("Download complete!");
-        }
-      });
-    });
-  }
+chrome.action.onClicked.addListener(function(tab) {
+  chrome.tabs.create({ url: "popup.html" });
 });
-
-
-const api = 'http://localhost:3000/xcontent?file=1&url='
-const downloadUrl = (url) => {
-    if (!url.startsWith('https://truyensextv.me/')) {
-        return null;
-    }
-    return api + url
-}
---
+++ option.js
@@ -0,0 +1,9 @@
+const detaultOption = {
+  domain: "youtube.com",
+  period_day: 30,
+  projects: [
+    "REALESTATE",
+    "GENERAL",
+    "SHP"
+  ]
+}
